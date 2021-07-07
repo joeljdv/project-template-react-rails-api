e8338ac9e8e602f847cdc9b6fdbc45f78498666b
joeljdv <joeldiazjdv@gmail.com>
2021-07-07 16:31:41 -0400

    Add link to signup, login and logout to Navbar

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { NavLink, Link } from 'react-router-dom'
+ const Navbar = (props) => {
+
+    if (props.loggedIn) {
+        return (
+            <Link>
+                <button>Signup</button>
+            </Link>
+        )
+    }else {
+        return (
+            <div>
+                <Link to="/signup">
+                    <button>Signup</button>
+                </Link>
+                <Link to='/login'>
+                    <button>Login</button>
+                </Link>
+            </div>
+    ) 
+    }
+
+   
+}
+
+export default Navbar
