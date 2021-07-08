dccfbf5bdb15c4c044374decb15558dba0e43acb
joeljdv <joeldiazjdv@gmail.com>
2021-07-07 23:20:58 -0400

    create cars controller methods

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,2 +1,25 @@
 class CarsController < ApplicationController
+    before_action :authorize
+
+    def index
+        user = User.find_by(id: session[:user_id])
+        cars = user.cars
+        render json: cars
+    end
+
+    def create
+        user = User.find_by(id: session[:user_id])
+        cars = user.cars.create(cars_params)
+        render json: cars, status: :created
+    end
+
+    private
+
+    def cars_params
+        params.permit(:name, :make, :year, :image)
+    end
+
+    def authorize
+        return render json: {error: "Not authorized"}, status: :unauthorized unless session.include? :user_id
+    end
 end
