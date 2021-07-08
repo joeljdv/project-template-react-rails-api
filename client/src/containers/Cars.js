95e38062a8ff4439e5d42a26c2b64e1c4d366f86
joeljdv <joeldiazjdv@gmail.com>
2021-07-07 23:43:32 -0400

    write funtions to render cars in the cars file

---
+++ client/src/containers/Cars.js
@@ -0,0 +1,67 @@
+import React, {useState, useEffect} from 'react'
+import CarsForm from './CarsForm'
+
+
+const Cars = () => {
+    const [cars, setCars] = useState([])
+    const [error, setError] = useState("")
+    const [formFlag, setFormFlag] = useState(false)
+
+    useEffect( () => {
+        fetch('/cars')
+        .then(r => r.json())
+        .then(data => {
+            console.log(data)
+            if(data){
+                if(data.error){
+                    setError(data.error)
+                } else {
+                    setCars(cars)
+                }
+            }else {
+                setError("Not Authorized")
+            }
+        })
+    }, [])
+
+    const addCar = (car) => {
+        fetch('/cars', {
+            method: "POST",
+            headers: {
+                "Content-Type":"application/json"
+            },
+            body: JSON.stringify(car)
+        })
+        .then(r => r.json)
+        .then(data => {
+            setCars([...cars, data])
+            setFormFlag(false)
+        })
+    }
+
+    const carsList = cars.map(car => <div><li key={car.id}>{car.name}</li><br/></div>)
+
+    if (error === '') {
+        return (
+            <div>
+                <ul>
+                    {carsList}
+                    {formFlag ?
+                        <CarsForm addCar={addCar}/> :
+                        <button onClick={() => {setFormFlag(true)}}>Add car</button>
+                    }
+                </ul>
+            </div>
+        )
+    }else {
+        return (
+            <div>   
+                <h3>Not authorized</h3>
+                <h2>Sign up or Login</h2>   
+             </div>
+        )
+    }
+    
+}
+
+export default Cars
