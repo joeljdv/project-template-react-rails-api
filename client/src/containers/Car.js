14809b576e8f345e62cc6416f20a62b7d0a96131
joeljdv <joeldiazjdv@gmail.com>
2021-07-08 13:31:40 -0400

    Render car information when a car link is clicked

--- client/src/containers/Car.js
+++ client/src/containers/Car.js
@@ -1,11 +1,27 @@
 import React, {useState, useEffect} from 'react'
 
 
-const Car = () => {
+const Car = (props) => {
+
+    const [car, setCar] = useState({})
+    const [error, setError] = useState("")
+
+    useEffect( () => {
+    fetch(`/cars/${props.match.params.id}`)
+    .then(r => r.json())
+    .then(data => {
+        console.log(data)
+        setCar(data)
+        })
+    }, [] )
+
     return (
         <div>
-            
+            <img src={car.image} class='car_img'/>
+            <h2>{car.name}</h2>
+            <p><strong>Make:</strong> {car.make}</p>
+            <p><strong>Year:</strong> {car.year}</p>
         </div>
-    )
+        )
 }
 export default Car
